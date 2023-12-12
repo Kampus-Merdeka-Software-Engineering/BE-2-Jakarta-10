32ebc09cd71f0dfb4a1fda5737e693b1235c4ce0
Fawwas17 <fawwsshafiq36@gmail.com>
2023-12-12 22:10:24 +0700

    update

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const processController = require("../controllers/processController");
 const router = express.Router();
-router.post("/payment", processController.Paymentt);
+router.post("/payment", processController.Payment);
 
 module.exports = router;
 
