0ac31246f287a123550218bc67e4409be7a2e5a8
RM-52 <96343171+RM-52@users.noreply.github.com>
2023-12-13 12:00:56 +0700

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
   dialect: "mysql",
   host: process.env.DB_HOST || "roundhouse.proxy.rlwy.net",
-  port: process.env.DB_PORT || "20498",
+  port: process.env.DB_PORT || "22564",
   username: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "b-2-Acgcg5Gd31fgfgFAh3B3EGdhG21C",
   database: process.env.DB_NAME || "railway",
