f5e6801b7124cac96ad88dabcc399100d1b27593
Fawwas17 <fawwsshafiq36@gmail.com>
2023-12-13 13:16:23 +0700

    update

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,25 +1,30 @@
-
-
 // src/utils/db.js
 const { Sequelize } = require('sequelize');
 
-
 const sequelize = new Sequelize({
-  dialect: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306,
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'ruhuldatabase',
+  dialect: "mysql",
+  host: process.env.DB_HOST || "roundhouse.proxy.rlwy.net",
+  port: process.env.DB_PORT || "22564",
+
+  username: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "HGCbe-hFBb6BC-ddb32GDbedeEDhACba",
+  database: process.env.DB_NAME || "railway",
 });
 
-module.exports = sequelize;
-// src/utils/db.js
-sequelize
-  .authenticate()
-  .then(() => {
+// Function to connect to the database
+async function connectToDatabase() {
+  try {
+    await sequelize.authenticate();
     console.log('Connection to the database has been established successfully.');
-  })
-  .catch((err) => {
+  } catch (err) {
     console.error('Unable to connect to the database:', err);
-  });
+    // Handle the error or throw it to be caught elsewhere in your application
+    throw err;
+  }
+}
+
+// Call the function to connect to the database
+connectToDatabase();
+
+// Export the sequelize instance
+module.exports = sequelize;
