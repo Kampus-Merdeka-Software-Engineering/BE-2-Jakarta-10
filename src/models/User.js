7209587cf9422ecdf0e4783c16832afc7fe3093a
Fawwas17 <fawwsshafiq36@gmail.com>
2023-12-13 13:31:55 +0700

    update

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,7 @@
 // src/models/User.js
 const { DataTypes } = require("sequelize");
-<<<<<<< HEAD
-const sequelize = require("../\/utils/db");
-=======
 const sequelize = require("../utils/db");
->>>>>>> 73f05c4bebbbeec9947a6897a9cf53e4acd9696e
+const sequelize = require("../utils/db");
 
 const User = sequelize.define(
   "User",
--- src/utils/db.js
+++ src/utils/db.js
@@ -4,17 +4,10 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
   dialect: "mysql",
   host: process.env.DB_HOST || "roundhouse.proxy.rlwy.net",
-<<<<<<< HEAD
   port: process.env.DB_PORT || "22564",
 
   username: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "HGCbe-hFBb6BC-ddb32GDbedeEDhACba",
-=======
-  port: process.env.DB_PORT || "20498",
-
-  username: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "b-2-Acgcg5Gd31fgfgFAh3B3EGdhG21C",
->>>>>>> 73f05c4bebbbeec9947a6897a9cf53e4acd9696e
   database: process.env.DB_NAME || "railway",
 });
 
@@ -34,8 +27,4 @@ async function connectToDatabase() {
 connectToDatabase();
 
 // Export the sequelize instance
-<<<<<<< HEAD
 module.exports = sequelize;
-=======
-module.exports = sequelize;
->>>>>>> 73f05c4bebbbeec9947a6897a9cf53e4acd9696e
