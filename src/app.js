2e6ed080db4e0bfd92e5daa046f9c2e0467d07e5
RM-52 <96343171+RM-52@users.noreply.github.com>
2023-12-13 15:25:53 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,12 +14,12 @@ app.use(bodyParser.json());
 // Routes
 app.use("/auth", authRoutes);
 app.use("/process", paymentRoutes); // Gunakan route untuk pembayaran
-const corsOptions = {
-  origin: "https://kampus-merdeka-software-engineering.github.io/FE-2-Jakarta-10/Page/Registrasi.html", // Ganti dengan URL front end Anda
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true, // Mengizinkan pengiriman cookie melalui CORS
-  optionsSuccessStatus: 204, // Memberikan status 204 untuk metode OPTIONS
-};
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Atur domain atau '*' untuk memungkinkan dari semua domain
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
