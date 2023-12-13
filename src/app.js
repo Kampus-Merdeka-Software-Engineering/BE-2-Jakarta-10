41ee7b6f74baddca249820b8e35b63ecbf6b5d4a
RM-52 <96343171+RM-52@users.noreply.github.com>
2023-12-13 11:49:47 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,17 +4,28 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes"); // Sesuaikan dengan path yang benar
 const paymentRoutes = require("./routes/paymentRoutes");
 const app = express();
-const PORT = 22564;
+const PORT = 20498;
 
 // Middleware CORS
 app.use(cors());
 
 app.use(bodyParser.json());
 
+
 // Routes
 app.use("/auth", authRoutes);
 app.use("/process", paymentRoutes); // Gunakan route untuk pembayaran
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Middleware untuk menangkap kesalahan
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
 });
+app.listen(PORT, async () => {
+  try {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  } catch (error) {
+    console.error("Error starting the server:", error);
+  }
+});
+
+
