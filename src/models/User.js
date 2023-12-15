352dbb41d6e38ae8fbc2b21c4502ed74c22722da
Fawwas17 <fawwsshafiq36@gmail.com>
2023-12-15 23:30:06 +0700

    update

--- src/models/User.js
+++ src/models/User.js
@@ -19,14 +19,6 @@ const User = sequelize.define(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    createdAt: {
-      type: DataTypes.DATE,
-      field: "created_at", 
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      field: "updated_at", 
-    },
   },
   {
     timestamps: true,
